f5e5751dc0f493521b8faf2fba40073f0b180f32
Zach Klippenstein <zachklipp@squareup.com>
2019-04-12 12:09:19 -0700

    Upgrade kotlin and coroutines dependencies to latest versions.
    Closes #188 because the deprecated `cancel` method that took `Throwable` has been removed.
    
    The issue where workflow subscriptions were disposed multiple times also seems to have been fixed.
    
    The only real thing this "broke" was how legacy workflow's `switchMapState` handles channel cancellation,
    now we have to be a bit more explicit. This is why channels shouldn't be used as streams…

--- kotlin/workflow-testing/src/main/java/com/squareup/workflow/testing/WorkflowTesting.kt
+++ kotlin/workflow-testing/src/main/java/com/squareup/workflow/testing/WorkflowTesting.kt
@@ -22,6 +22,7 @@ import com.squareup.workflow.StatefulWorkflow
 import com.squareup.workflow.Workflow
 import com.squareup.workflow.WorkflowHost
 import com.squareup.workflow.WorkflowHost.Factory
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.Job
@@ -112,15 +113,13 @@ private fun <T, O : Any, R : Any> test(
     throw e
   } finally {
     if (error != null) {
-      // TODO https://github.com/square/workflow/issues/188 Stop using parameterized cancel.
-      @Suppress("DEPRECATION")
-      val cancelled = context.cancel(error)
-      if (!cancelled) {
-        val cancellationCause = context[Job]!!.getCancellationException()
-            .cause
-        if (cancellationCause != error && cancellationCause != null) {
-          error.addSuppressed(cancellationCause)
-        }
+      context.cancel(
+          if (error is CancellationException) error else CancellationException(null, error)
+      )
+      val cancellationCause = context[Job]!!.getCancellationException()
+          .cause
+      if (cancellationCause != error && cancellationCause != null) {
+        error.addSuppressed(cancellationCause)
       }
     } else {
       // Cancel the Job to ensure everything gets cleaned up.
